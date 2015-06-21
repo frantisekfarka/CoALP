584c65bc970f96ae0d132cf2a7276d2919ba57cc
frantisekfarka <frantisek@farka.eu>
2015-06-21 22:52:55 +0100

    reduction helpers

---
+++ src/CoALP/Reductions.hs
@@ -0,0 +1,36 @@
+module CoALP.Reductions (
+	 isVarReductOf
+	,nvPropSub
+) where 
+
+import Control.Arrow ((***))
+import Data.Maybe (isJust)
+
+import CoALP.Program(Term(..))
+
+
+
+isVarReductOf :: Term a b c -> Term a b c -> Maybe (Term a b c)
+isVarReductOf (Fun id1 ts1) (Fun id2 ts2) 
+	| id1 == id2	= h (filter isJust $ zipWith isVarReductOf ts1 ts2)
+	| otherwise	= Nothing
+	where
+		h [] 	= Nothing
+		h (x:_)	= x
+isVarReductOf (Var _) mr@(Fun _ _) = Just mr
+isVarReductOf (Var _) (Var _) = Nothing
+isVarReductOf (Fun _ _) (Var _) = Nothing
+
+nvPropSub :: Term a b c -> [(Term a b c, [Int])]
+nvPropSub t = map (id *** reverse) (nvPropSub_ (t, [0]))
+
+
+nvPropSub_ :: (Term a b c, [Int]) -> [(Term a b c, [Int])]
+nvPropSub_ ((Fun _ ts),v)	= nvs ++ concatMap nvPropSub_ nvs
+	where
+		nvs = [(t, v':v) | (t@(Fun _ _), v') <- zip ts [0..]]
+nvPropSub_ ((Var _),_)		= []
+
+
+
+
