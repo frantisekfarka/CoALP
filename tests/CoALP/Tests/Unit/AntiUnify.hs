c820e01e5f83f958c13244568f4b67e8a088c6e6
arpond <a.r.pond@dundee.ac.uk>
2015-07-23 12:28:40 +0100

    Updated Tests

--- tests/CoALP/Tests/Unit/AntiUnify.hs
+++ tests/CoALP/Tests/Unit/AntiUnify.hs
@@ -4,7 +4,7 @@
 module CoALP.Tests.Unit.AntiUnify where
 
 import CoALP.AntiUnify
-import CoALP.Program (Term(..), Term1)
+import CoALP.Program (Term(..), Term1, TermA, AnnoVar(..))
 --import Data.List (intersperse)
 
 import Test.Tasty
@@ -37,6 +37,12 @@ antiUnifyTests = testGroup "Anti Unify tests" [
                     antiUnify f13 f14 @?= a7
             , testCase "Martin Example with starting count of 8" $
                     antiUnifyWithCount f11 f12 8 @?= a8
+            , testCase "Tom test with AnnoVar" $
+                    antiUnify f15 f16 @?= a9
+            , testCase "Martin example with AnnoVar" $
+                    antiUnify f17 f18 @?= a10
+            , testCase "Martin Example with starting count of 8 and AnnoVar" $
+                    antiUnifyWithCount f17 f18 (Ind 8) @?= a11
             ]
   where
             f1 = Fun "Eq" [Fun "D" [Fun "Char" []]] :: Term1
@@ -61,4 +67,11 @@ antiUnifyTests = testGroup "Anti Unify tests" [
             f14 = Fun "App" [Fun "Eq" [], Fun "App" [Fun "D" [], Fun "App" [Fun "D" [], Fun "Char" []]]] :: Term1
             a7  = Fun "App" [Fun "Eq" [], Fun "App" [Fun "D" [], Var 2]] :: Term1
             a8  = Fun "p" [Var 16, Var 32] :: Term1
+            f15 = Fun "App" [Fun "Eq" [], Fun "App" [Fun "D" [], Fun "Char" []]] :: TermA
+            f16 = Fun "App" [Fun "Eq" [], Fun "App" [Fun "D" [], Fun "App" [Fun "D" [], Fun "Char" []]]] :: TermA
+            a9  = Fun "App" [Fun "Eq" [], Fun "App" [Fun "D" [], Var (Ind 2)]] :: TermA
+            f17 = Fun "p" [Fun "a" [], Fun "a" []] :: TermA
+            f18 = Fun "p" [Fun "b" [], Fun "c" []] :: TermA
+            a10  = Fun "p" [Var (Ind 2), Var (Ind 4)]  :: TermA
+            a11  = Fun "p" [Var (Ind 16), Var (Ind 32)] :: TermA
             
