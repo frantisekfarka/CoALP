d218ae23c58601d9cfd5b5ff232eea07971dc21b
frantisekfarka <frantisek@farka.eu>
2015-08-26 14:31:12 +0100

    in-coind search scheme

--- src/CoALP/Sound.hs
+++ src/CoALP/Sound.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module CoALP.Sound (
 	res
 ) where
@@ -15,29 +17,52 @@ import CoALP.Program (
 	)
 
 import CoALP.DerTree (der,clauseProj)
+import CoALP.FreshVar (Freshable)
 import CoALP.Guards (guardingContext)
 
 import Debug.Trace
 
 
-res ::
-	Program Ident Constant Var
-	-> Signature Ident
-	-> Clause Ident Constant Var
-	-> [Succ Ident Constant Var]
-res p s c = resDerTree [] $ (der p c :: DerTree Ident Constant Var VR )
+-- | Resolve a query
+--
+res :: forall a b c . (Eq a, Eq b, Ord c, Freshable c) =>
+	Program a b c
+	-> Signature a
+	-> Clause a b c 
+	-> [Succ a b c]
+res p s c = resDerTree [] dt
+	where
+		dt :: DerTree a b c VR 
+		dt = der p c 
+
+-- | Process a tree and procees
+--
+resDerTree gcs (DT rt trs) = (indRes rt) ++
+	case separateTrs trs of
+		([], cotrs)	-> concatMap (resCoIndTrans gcs) cotrs
+		(indtrs, _)	-> concatMap (resIndTrans gcs) indtrs
 
 
-resDerTree gcs (DT rt trs) = (indRes rt) ++ (concatMap (resTrans gcs) trs)
+-- | Process a transition within a tree that sill has
+-- some unprocessed inductive obligations
+resIndTrans gcs (Trans p rt _ cx dt) = resDerTree (gc:gcs) dt
+	where
+		gc = guardingContext p rt cx 
 
-resTrans gcs (Trans p rt _ cx dt) = case indClosed && (not $ null gc) && (gc `elem` gcs) of
+-- | Process a transition within a tree that has no inductive
+-- obligations - therefore we can conclude coinductively
+resCoIndTrans gcs (Trans p rt _ cx dt) = case (not $ null gc) && (gc `elem` gcs) of
 		True	-> [rep rt]
 		False	-> resDerTree (gc:gcs) dt
 	where
 		gc = guardingContext p rt cx 
 		rep (RTEmpty) = error "impossible"
 		rep (RT c s _) = CoIndS c gc
-		indClosed = False
+
+-- | Separate transitions into inductive and coinductive obligations
+separateTrs trs = ([], trs)
+
+
 
 
 -- TODO make into traversal over the tree
